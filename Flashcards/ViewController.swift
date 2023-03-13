37dc4273f8aa0db5b8fbbf5d6c3523060ca7cd96
RafiaSafina <safinarafia@gmail.com>
2023-03-12 17:31:28 -0700

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Рафия Сафина on 12.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
